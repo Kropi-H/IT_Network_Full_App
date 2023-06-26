33e1c2d8076503242d54eee967bb563730b3769f
kropi-h <kropi-h@github.com>
2023-06-26 21:47:49 +0200

    pridani pojistek a jejich přiřazení. začátek úpravy ojistek uživatelem

--- app.py
+++ app.py
@@ -19,6 +19,43 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+
+@app.route('/delete_pojisteni/<int:id>', methods=['POST', 'GET'])
+def delete_pojisteni(id):
+    db = get_db()
+    if request.method == 'GET':
+        cur = db.execute('SELECT * FROM pojistky WHERE id=?', [id])
+        result = cur.fetchone()
+        db.execute('DELETE FROM pojistky WHERE id=?', [id])
+        db.commit()
+        return redirect(url_for('pojistenci_detail', id = result['id_pojistence']))
+
+@app.route('/uprava_pojisteni/<int:id>', methods=['POST', 'GET'])
+def uprava_pojisteni(id):
+    db = get_db()
+    if request.method == 'GET':
+        cur = db.execute('SELECT * FROM pojistky WHERE id=?', [id])
+        result = cur.fetchone()
+
+        pojistenec_cur = db.execute('SELECT * FROM pojistenci WHERE id=?', [result['id_pojistence']])
+        result_pojistenec = pojistenec_cur.fetchone()
+
+        pojisteni_cur = db.execute('SELECT predmet_pojisteni FROM pojisteni')
+        result_pojisteni = pojisteni_cur.fetchall()
+
+        return render_template('uprava_pojisteni.html', pojistka = result, pojisteni = result_pojisteni, pojistenec = result_pojistenec)
+
+    elif request.method == 'POST':
+        id_pojistence = id
+        typ_pojisteni = request.form.get('pojisteni')
+        hodnota_pojisteni = request.form['castka']
+        predmet_pojisteni = request.form['predmet_pojisteni']
+        platnost_od = request.form['platnost_od']
+        platnost_do = request.form['platnost_do']
+        db.execute('UPDATE pojistky SET predmet_pojisteni = ?, typ_pojisteni = ?, castka = ?, platnost_od = ?, platnost_do = ?  WHERE id = ?',[typ_pojisteni, predmet_pojisteni, hodnota_pojisteni, platnost_od, platnost_do, id_pojistence])
+        db.commit()
+        return redirect(url_for('pojistenci_detail', id = id_pojistence))
+
 @app.route('/')
 def index():
     return render_template('index.html', page_title = 'Hlavní stránka')
@@ -46,13 +83,13 @@ def get_member(id):
 
 @app.route('/pojistenec_update/<int:id>', methods=['POST', 'GET'])
 def pojistenec_update(id):
-    db = connect_db()
+    db = get_db()
     cur = db.execute('SELECT * FROM pojistenci WHERE id=?', [id])
     result = cur.fetchone()
     header = 'Úprava dat pojištěnce {}'.format(result['prijmeni'])
 
     if request.method == 'POST':
-        id = id
+        id_user = id
         jmeno = request.form['first_name']
         prijmeni = request.form['second_name']
         email = request.form['email']
@@ -60,21 +97,37 @@ def pojistenec_update(id):
         adresa = request.form['street']
         mesto = request.form['town']
         psc = request.form['psc']
-        db.execute('UPDATE pojistenci SET jmeno = ?, prijmeni = ?, email = ?, telefon = ?, adresa = ?, mesto = ?, psc = ?  WHERE id = ?',[jmeno, prijmeni, email, telefon, adresa, mesto, psc, id])
+        db.execute('UPDATE pojistenci SET jmeno = ?, prijmeni = ?, email = ?, telefon = ?, adresa = ?, mesto = ?, psc = ?  WHERE id = ?',[jmeno, prijmeni, email, telefon, adresa, mesto, psc, id_user])
         db.commit()
-        return redirect('/pojistenci')
+        return redirect(url_for('pojistenci_detail', id = id_user))
 
     return render_template('novy_pojistenec.html', page_title = 'Pojistenec {} úprava'.format(result['prijmeni']), edit_data = result, header=header)
 
 
 @app.route('/pojisteni/<int:id>', methods=['POST', 'GET'])
 def pojisteni(id):
-    db = connect_db()
-    cur = db.execute('SELECT pojisteni FROM typ_pojisteni')
+    db = get_db()
+
+    cur = db.execute('SELECT predmet_pojisteni FROM pojisteni')
     result = cur.fetchall()
 
     cur_pojistenec = db.execute('SELECT id, jmeno, prijmeni FROM pojistenci WHERE id=?', [id])
     result_pojistenec = cur_pojistenec.fetchone()
+
+    if request.method == 'POST':
+        id_pojistence = id
+        typ_pojisteni = request.form.get('pojisteni')
+        hodnota_pojisteni = request.form['castka']
+        predmet_pojisteni = request.form['predmet_pojisteni']
+        platnost_od = request.form['platnost_od']
+        platnost_do = request.form['platnost_do']
+
+
+        db.execute('INSERT INTO pojistky (id_pojistence, predmet_pojisteni, typ_pojisteni, castka, platnost_od, platnost_do ) VALUES (?,?,?,?,?,?)',
+                   [id_pojistence, predmet_pojisteni, typ_pojisteni,  hodnota_pojisteni, platnost_od, platnost_do ])
+        db.commit()
+
+        return redirect(url_for('pojistenci_detail', id = id_pojistence))
 
     return render_template('pojisteni.html', page_title = 'Pojištění', pojisteni = result, pojistenec=result_pojistenec)
 
@@ -87,13 +140,15 @@ def pojistenci_detail(id):
     db=get_db()
     cur = db.execute('SELECT * FROM pojistenci WHERE id=?', [id])
     result = cur.fetchone()
-    return render_template('pojistenec_detail.html', page_title='Pojištěnec {}'.format(result['prijmeni']), data = result)
+    pojisteni_cur = db.execute('SELECT * FROM pojistky WHERE id_pojistence=?', [id])
+    pojisteni_result = pojisteni_cur.fetchall()
+
+    return render_template('pojistenec_detail.html', page_title='Pojištěnec {}'.format(result['prijmeni']), data = result, pojisteni = pojisteni_result)
 
 @app.route('/pojistenci/novy', methods = ['POST','GET'])
 def pojistenci_novy():
     db = get_db()
     if request.method == 'POST':
-
         jmeno = request.form['first_name']
         prijmeni = request.form['second_name']
         email = request.form['email']
@@ -104,6 +159,7 @@ def pojistenci_novy():
         db.execute('INSERT INTO pojistenci (jmeno, prijmeni, email, adresa, mesto, psc, telefon, email) VALUES (?,?,?,?,?,?,?,?)',
                 [jmeno, prijmeni, email, adresa, mesto, psc, telefon, email])
         db.commit()
+        return redirect(url_for('pojistenci'))
 
     return render_template('novy_pojistenec.html', page_title = 'Nový pojištěnec', header = 'Nový pojištěnec')
 
--- pojisteni_app.sql
+++ pojisteni_app.sql
@@ -1,23 +1,25 @@
 create table pojistenci (
     id integer primary key autoincrement,
-    jmeno integer not null,
-    prijmeni  integer not null,
-    adresa  integer not null,
-    mesto  integer not null,
+    jmeno text not null,
+    prijmeni  text not null,
+    adresa  text not null,
+    mesto  text not null,
     psc  integer not null,
     telefon  integer not null,
-    email  integer not null
+    email  text not null
 );
 
-create table typ_pojisteni (
+create table pojistky (
     id integer primary key autoincrement,
-    pojisteni  integer not null
+    id_pojistence  integer not null,
+    predmet_pojisteni text not null,
+    typ_pojisteni text not null,
+    castka integer not null,
+    platnost_od text not null,
+    platnost_do text not null
 );
 
-create table pojisteni_pojistence (
+create table pojisteni (
     id integer primary key autoincrement,
-    castka  integer not null,
-    predmet_pojisteni  integer not null,
-    platnost_od  integer not null,
-    platnost_do  integer not null
+    predmet_pojisteni text not null
 );
